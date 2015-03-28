b2ee024d3bee6dc0b46998da735cc7fb40c4c93d
Jefferson Daniel <jeffersondaniels@hotmail.com>
2015-03-28 20:46:13 -0300

    issues codeclimate :white_check_mark:

--- app/index.js
+++ app/index.js
@@ -43,30 +43,30 @@ module.exports = yeoman.generators.Base.extend({
     },
 
     scaffoldFolders: function(){
-      this.mkdir("app");
-      this.mkdir("app/assets");
-      this.mkdir("app/assets/scripts");
-      this.mkdir("app/assets/styles");
-      this.mkdir("app/assets/styles/core");
-      this.mkdir("app/assets/styles/patterns");
-      this.mkdir("app/assets/fonts");
-      this.mkdir("app/assets/images");
+      this.mkdir('app');
+      this.mkdir('app/assets');
+      this.mkdir('app/assets/scripts');
+      this.mkdir('app/assets/styles');
+      this.mkdir('app/assets/styles/core');
+      this.mkdir('app/assets/styles/patterns');
+      this.mkdir('app/assets/fonts');
+      this.mkdir('app/assets/images');
     },
 
     projectfiles: function(){
-      this.copy("_index.html", "app/index.html");
-      this.copy("styles/_patterns.scss", "app/assets/styles/patterns/_patterns.scss");
-      this.copy("styles/_core.scss", "app/assets/styles/core/_core.scss");
-      this.copy("styles/_normalize.scss", "app/assets/styles/core/_normalize.scss");
-      this.copy("styles/_normalize.scss", "app/assets/styles/core/_normalize.scss");
-      this.copy("styles/_variables.scss", "app/assets/styles/core/_variables.scss");
-      this.copy("styles/_main.scss", "app/assets/styles/main.scss");
-      this.copy("scripts/_main.js", "app/assets/scripts/main.js");
+      this.copy('_index.html', 'app/index.html');
+      this.copy('styles/_patterns.scss', 'app/assets/styles/patterns/_patterns.scss');
+      this.copy('styles/_core.scss', 'app/assets/styles/core/_core.scss');
+      this.copy('styles/_normalize.scss', 'app/assets/styles/core/_normalize.scss');
+      this.copy('styles/_normalize.scss', 'app/assets/styles/core/_normalize.scss');
+      this.copy('styles/_variables.scss', 'app/assets/styles/core/_variables.scss');
+      this.copy('styles/_main.scss', 'app/assets/styles/main.scss');
+      this.copy('scripts/_main.js', 'app/assets/scripts/main.js');
 
-      this.copy("_gruntfile.js", "Gruntfile.js");
-      this.copy("editorconfig", ".editorconfig");     
-      this.copy("jshintrc", ".jshintrc");
-      this.copy("_gitignore", ".gitignore");
+      this.copy('_gruntfile.js', 'Gruntfile.js');
+      this.copy('editorconfig', '.editorconfig');
+      this.copy('jshintrc', '.jshintrc');
+      this.copy('_gitignore', '.gitignore');
     }
   },
 
